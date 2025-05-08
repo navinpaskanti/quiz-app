41a9742bdf7a10e907fc9a6b0c48d249d46b6fbc
Navin Paskanti <117461033+navinpaskanti@users.noreply.github.com>
2025-05-08 16:42:29 +0530

    first commit

--- src/questions.js
+++ src/questions.js
@@ -1,20 +1,39 @@
 const questions = [
-    {
-      question: "What is the capital of France?",
-      options: ["Paris", "London", "Berlin", "Madrid"],
-      answer: "Paris",
-    },
-    {
-      question: "What is 5 + 3?",
-      options: ["5", "8", "12", "15"],
-      answer: "8",
-    },
-    {
-      question: "Who wrote 'Hamlet'?",
-      options: ["Shakespeare", "Hemingway", "Dickens", "Austen"],
-      answer: "Shakespeare",
-    },
-  ];
-  
-  export default questions;
-  
+  {
+    question: "What is the capital of France?",
+    options: ["Paris", "London", "Berlin", "Madrid"],
+    answer: "Paris",
+  },
+  {
+    question: "What is 5 + 3?",
+    options: ["5", "8", "12", "15"],
+    answer: "8",
+  },
+  {
+    question: "Who wrote 'Hamlet'?",
+    options: ["Shakespeare", "Hemingway", "Dickens", "Austen"],
+    answer: "Shakespeare",
+  },
+  {
+    question: "The country that has the highest in Barley Production?",
+    options: ["China", "India", "Russia", "France"],
+    answer: "Russia",
+  },
+  {
+    question: "For which of the following disciplines is Nobel Prize awarded?",
+    options: [
+      "All of the above",
+      "Physiology or Medicine",
+      "Literature, Peace and Economics",
+      "Physics and Chemistry",
+    ],
+    answer: "All of the above",
+  },
+  {
+    question: "Epsom (England) is the place associated with",
+    options: ["Polo", "Snooker", "Shooting", "Horse racing"],
+    answer: "Horse racing",
+  },
+];
+
+export default questions;
